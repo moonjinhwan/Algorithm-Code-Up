cdd84b159f9a165724225672e8f7a7282aa12a6f
moonjinhwan <moonjinh123@naver.com>
2020-04-14 17:56:56 +0900

    개미

---
+++ back_Joon/simulation/4307 개미.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+int main() {
+	int TEST;
+	cin >> TEST;
+	for (int t = 0; t < TEST; t++) {
+		int N, L;
+		cin >> N >> L;
+		vector<int> min_time;
+		vector<int> max_time;
+		for (int i = 0; i < L; i++) {
+			int a;
+			cin >> a;
+			min_time.push_back(min(a, N - a));
+			max_time.push_back(max(a, N - a));
+		}
+		cout << *max_element(min_time.begin(), min_time.end())<<' ';
+		cout << *max_element(max_time.begin(), max_time.end())<<endl;
+	}
+}
