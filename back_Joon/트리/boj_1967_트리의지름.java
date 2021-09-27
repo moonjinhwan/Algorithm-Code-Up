a67c1b3445044fc2efa197310d1282f96af0875b
moonjinh <moonjinh123@naver.com>
2021-09-27 23:36:29 +0900

    9.27

--- back_Joon/트리/boj_1967_트리의지름.java
+++ back_Joon/트리/boj_1967_트리의지름.java
@@ -1,25 +1,22 @@
 import java.io.*;
 import java.util.*;
 
-public class boj_1967_트리의지름
-{
+public class boj_1967_트리의지름 {
 	static int N, dp[];
 	static List<int[]> list[];
 	static boolean V[];
-	public static void main(String[] args) throws IOException
-	{
+
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		N = stoi(st.nextToken());
-		V=new boolean[N+1];
-		dp=new int[N+1];
+		V = new boolean[N + 1];
+		dp = new int[N + 1];
 		list = new ArrayList[N + 1];
-		for (int i = 0; i <= N; i++)
-		{
+		for (int i = 0; i <= N; i++) {
 			list[i] = new ArrayList<>();
 		}
-		for (int i = 0; i < N-1; i++)
-		{
+		for (int i = 0; i < N - 1; i++) {
 			st = new StringTokenizer(br.readLine());
 			int a = stoi(st.nextToken());
 			int b = stoi(st.nextToken());
@@ -27,32 +24,32 @@ public class boj_1967_트리의지름
 			list[a].add(new int[] { b, c });
 			list[b].add(new int[] { a, c });
 		}
-		V[1]=true;
+		V[1] = true;
 		dfs(1, 0);
 		int m = Arrays.stream(dp).max().getAsInt();
 		System.out.println(m);
 	}
+
 	static int dfs(int n, int w) {
-		int maxone =0;
-		int maxtwo =0;
-		for(int next[]: list[n]) {
-			if(!V[next[0]]) {
-				V[next[0]]=true;
+		int maxone = 0;
+		int maxtwo = 0;
+		for (int next[] : list[n]) {
+			if (!V[next[0]]) {
+				V[next[0]] = true;
 				int temp = dfs(next[0], next[1]);
-				if(maxone < temp) {
+				if (maxone < temp) {
 					maxtwo = maxone;
 					maxone = temp;
-				}
-				else if(maxone >= temp && maxtwo < temp) {
+				} else if (maxone >= temp && maxtwo < temp) {
 					maxtwo = temp;
 				}
 			}
 		}
 		dp[n] = maxone + maxtwo;
-		return w+ maxone;
+		return w + maxone;
 	}
-	static int stoi(String s)
-	{
+
+	static int stoi(String s) {
 		return Integer.valueOf(s);
 	}
 }
